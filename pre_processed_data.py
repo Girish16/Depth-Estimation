787a80e3006939ab27a58a7342357e4f907f7272
Girish16 <54175783+Girish16@users.noreply.github.com>
2023-01-28 10:05:36 +0000

    Update pre_processed_data.py
    This script defines the data path as the directory containing the KITTI dataset. Then it creates a dataset object that contains the file names of all the PNG images in the data path. Next, it defines a function that loads the images and depth maps using tf.io.read_file() to read the image and depth map files, tf.image.decode_png() to decode the image, tf.image.convert_image_dtype() to convert the image dtype to float32. The depth map is loaded similarly.
    
    It then applies the map() function to the dataset object to resize the images and depth maps using 'tf.image.resize()', and normalize the images using the formula: (image - 0.5) * 2 and depth map as well. Data augmentation is done by applying `tf.image.random_flip_left_right()'

--- pre_processed_data.py
+++ pre_processed_data.py
@@ -1,105 +1,38 @@
+import tensorflow as tf
 
-import numpy as np
-import cv2 as cv
-import os
-import pickle   
+# Define the data path
+data_path = 'path/to/kitti/dataset'
 
-path,dir,file1=next(os.walk(r"C:\Users\giris\OneDrive\Desktop\project\New folder\2011_09_26\2011_09_26_drive_0001_sync\image_00\data"))
-'Input of first five images are eliminated in all sequences'
-image1=np.zeros((len(file1)+1,192,640,3));
-for i in range(5,len(file1)-4):
-     base=r'C:\Users\giris\OneDrive\Desktop\project\New folder\2011_09_26\2011_09_26_drive_0001_sync\image_00\data'
-     p1=os.path.join(base,file1[i])
-     img1 = cv.imread(p1) 
-     img1=cv.resize(img1,(640,192))
-     image1[i-5,:,:,:]=img1;
-image1=image1[5:len(file1)-5,]
+# Create a dataset from the image files
+data_set = tf.data.Dataset.list_files(data_path + '/*.png')
 
-paths,dir,file2=next(os.walk(r"C:\Users\giris\Videos\Project\Train\2011_09_26_drive_0009_sync\image_00\data"))    
-image2=np.zeros((len(file2)+1,192,640,3));
-for i in range(5,439):
-     base=r'C:\Users\giris\Videos\Project\Train\2011_09_26_drive_0009_sync\image_00\data'
-     p2=os.path.join(base,file2[i])
-     img2 = cv.imread(p2) 
-     img2=cv.resize(img2,(640,192))
-     image2[i-5,:,:,:]=img2;
-image2=image2[5:439,]
+# Load the images and depth maps
+def load_image_and_depth(file):
+    image = tf.io.read_file(file)
+    image = tf.image.decode_png(image)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    depth_map = tf.io.read_file(file.replace('image', 'depth'))
+    depth_map = tf.image.decode_png(depth_map)
+    depth_map = tf.image.convert_image_dtype(depth_map, tf.float32)
+    return image, depth_map
 
- 
-x=int(len(file1)+len(file2)-20)
-img=np.zeros((x,375,1242,3))
-img=np.concatenate((image1,image2),axis=0)
-'https://www.datacamp.com/community/tutorials/pickle-python-tutorial'
-with open("t1.p","wb") as f:
-    pickle.dump(img,f)
-    f.close()
-x=len(file1)+len(file2)-20
-img=np.zeros((x,375,1242,3))
-img=np.concatenate((image1,image2),axis=0)
-with open("v.p","wb") as f:
-    pickle.dump(img,f)
-    f.close()
+data_set = data_set.map(load_image_and_depth)
 
-img.shape
+# Resize the images and depth maps
+def resize_image_and_depth(image, depth_map):
+    image = tf.image.resize(image, (128, 256))
+    depth_map = tf.image.resize(depth_map, (128, 256))
+    return image, depth_map
 
-path,dirss,file3=next(os.walk(r"C:\Users\giris\Videos\Depth\train\2011_09_26_drive_0001_sync\proj_depth\groundtruth\image_02"))   
-    
-image3=np.zeros((len(file3),192,640,3));
-for i in range(0,len(file3)):
-    base=r'C:\Users\giris\Videos\Depth\train\2011_09_26_drive_0001_sync\proj_depth\velodyne_raw\image_02'
-    p3=os.path.join(base,file3[i])
-    img3 = cv.imread(p3) 
-    img3=cv.resize(img3,(640,192))
-    image3[i,:,:,:]=img3;
+data_set = data_set.map(resize_image_and_depth)
 
+# Normalize the images
+def normalize_image(image, depth_map):
+    image = (image - 0.5) * 2
+    depth_map = (depth_map - 0.5) * 2
+    return image, depth_map
 
-path,dirss,file4=next(os.walk(r"C:\Users\giris\Videos\Project\Data\train\2011_09_26_drive_0009_sync\proj_depth\groundtruth\image_02"))    
-image4=np.zeros((len(file4)+192,640,3));
-for i in range(0,len(file4)):
-     base=r'C:\Users\giris\Videos\Project\Data\train\2011_09_26_drive_0009_sync\proj_depth\groundtruth\image_02'
-     p4=os.path.join(base,file4[i])
-     img4 = cv.imread(p4) 
-     img4=cv.resize(img4,(640,192))
+data_set = data_set.map(normalize_image)
 
-     image4[i,:,:,:]=img4;
- 
-    
-x=len(file3)+len(file4)
-img=np.zeros((x,375,1242,3))
-img=np.concatenate((image3,image4),axis=0)
-'https://www.datacamp.com/community/tutorials/pickle-python-tutorial'  
-with open("g1.p","wb") as f:
-    pickle.dump(img,f)
-    f.close()
-
-img.shape
-
-path,dirss,file5=next(os.walk(r"C:\Users\giris\Videos\Project\Train\2011_09_26_drive_0011_sync\image_00\data"))    
-image5=np.zeros((len(file5),192,640,3));
-for i in range(5,len(file5)-4):
-      base=r'C:\Users\giris\Videos\Project\Train\2011_09_26_drive_0011_sync\image_00\data'
-      p5=os.path.join(base,file5[i])
-      img5 = cv.imread(p5) 
-      img5=cv.resize(img5,(640,192))
-
-      image5[i-5,:,:,:]=img5;
-image5=image5[5:len(file5)-5,]
- 
-'https://www.datacamp.com/community/tutorials/pickle-python-tutorial'
-with open("t25.p","wb") as f:
-    pickle.dump(image5,f)
-    f.close()
-
-path,dirss,file6=next(os.walk(r"C:\Users\giris\Videos\Project\Data\train\2011_09_26_drive_0011_sync\proj_depth\groundtruth\image_02"))   
-image6=np.zeros((len(file6),375,1242,3));
-for i in range(0,len(file6)):
-      base=r'C:\Users\giris\Videos\Project\Data\train\2011_09_26_drive_0011_sync\proj_depth\groundtruth\image_02'
-      p6=os.path.join(base,file6[i])
-      img6 = cv.imread(p6) 
-      image6[i,:,:,:]=img6;
-'https://www.datacamp.com/community/tutorials/pickle-python-tutorial'      
-with open("g25.p","wb") as f:
-    pickle.dump(image6,f)
-    f.close()
-
-'similarly all the files are pre-processed and stored as pickle files'
+# Data augmentation
+data_set = data_set.map(lambda x, y: (tf.image.random_flip_left_right(x), tf.image.random_flip_left_right(y)))
